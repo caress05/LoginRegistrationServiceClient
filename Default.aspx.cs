cded2361cafadb4d1bb0c443d4f947bc3d870be7
caress05 <caress_roche@yahoo.com>
2016-02-17 11:38:22 -0800

    CommitClient

---
+++ Default.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void LoginButton_Click(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void VenueLogin()
+    {
+        ServiceReference.LoginServiceClient lsc = new ServiceReference.LoginServiceClient();
+        int key = lsc.VenueLogin(PasswordTextBox.Text, UserNameTextBox.Text);
+        if (key != -1)
+        {
+            ErrorLabel.Text = "Welcome";
+            Session["UserKey"] = key;
+        }
+        else
+        {
+            ErrorLabel.Text = "Login Failed";
+        }
+    }
+}
